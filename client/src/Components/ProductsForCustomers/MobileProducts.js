d436b256b291fb5551c24abc103f20752fff689a
Shivam <127921762+Itachi7011@users.noreply.github.com>
2024-03-21 23:25:12 +0530

    Add files via upload

---
+++ client/src/Components/ProductsForCustomers/MobileProducts.js
@@ -0,0 +1,411 @@
+const MobileProducts = () => {
+  return (
+    <>
+      <h1 className="title_heading-card-mobile">Mobiles</h1>
+      <div className="card_main-div">
+      <main role="main">
+  <div class="product">
+    <figure>
+      <img src="https://raw.githubusercontent.com/itbruno/productpreview/master/assets/img/t-shirt.jpg" alt="Product" class="product-image"/>
+        </figure>
+
+      <div class="product-description">
+
+        <div class="info">
+          <h1>LOREM IPSUM</h1>
+          <p>
+            Lorem Ipsum is simply dummy
+            printing and typesetting industry
+          </p>
+        </div>
+
+        <div class="price">
+          89
+        </div>
+      </div>
+
+      <div class="product-sidebar">
+        <button class="buy">
+          <span>BUY ITEM</span>
+        </button>
+
+        <button class="info">
+          <span>MORE INFO</span>
+        </button>
+
+        <button class="size">
+          <span>SIZES</span>
+        </button>
+
+        <button class="colors">
+          <span>
+            <a href="/" class="color black"></a>
+            <a href="/" class="color white"></a>
+            <a href="/" class="color red"></a>
+          </span>
+        </button>
+      </div>
+      </div>
+    </main>
+    <main role="main">
+  <div class="product">
+    <figure>
+      <img src="https://raw.githubusercontent.com/itbruno/productpreview/master/assets/img/t-shirt.jpg" alt="Product" class="product-image"/>
+        </figure>
+
+      <div class="product-description">
+
+        <div class="info">
+          <h1>LOREM IPSUM</h1>
+          <p>
+            Lorem Ipsum is simply dummy
+            printing and typesetting industry
+          </p>
+        </div>
+
+        <div class="price">
+          89
+        </div>
+      </div>
+
+      <div class="product-sidebar">
+        <button class="buy">
+          <span>BUY ITEM</span>
+        </button>
+
+        <button class="info">
+          <span>MORE INFO</span>
+        </button>
+
+        <button class="size">
+          <span>SIZES</span>
+        </button>
+
+        <button class="colors">
+          <span>
+            <a href="/" class="color black"></a>
+            <a href="/" class="color white"></a>
+            <a href="/" class="color red"></a>
+          </span>
+        </button>
+      </div>
+      </div>
+    </main>
+    <main role="main">
+  <div class="product">
+    <figure>
+      <img src="https://raw.githubusercontent.com/itbruno/productpreview/master/assets/img/t-shirt.jpg" alt="Product" class="product-image"/>
+        </figure>
+
+      <div class="product-description">
+
+        <div class="info">
+          <h1>LOREM IPSUM</h1>
+          <p>
+            Lorem Ipsum is simply dummy
+            printing and typesetting industry
+          </p>
+        </div>
+
+        <div class="price">
+          89
+        </div>
+      </div>
+
+      <div class="product-sidebar">
+        <button class="buy">
+          <span>BUY ITEM</span>
+        </button>
+
+        <button class="info">
+          <span>MORE INFO</span>
+        </button>
+
+        <button class="size">
+          <span>SIZES</span>
+        </button>
+
+        <button class="colors">
+          <span>
+            <a href="/" class="color black"></a>
+            <a href="/" class="color white"></a>
+            <a href="/" class="color red"></a>
+          </span>
+        </button>
+      </div>
+      </div>
+    </main>
+    <main role="main">
+  <div class="product">
+    <figure>
+      <img src="https://raw.githubusercontent.com/itbruno/productpreview/master/assets/img/t-shirt.jpg" alt="Product" class="product-image"/>
+        </figure>
+
+      <div class="product-description">
+
+        <div class="info">
+          <h1>LOREM IPSUM</h1>
+          <p>
+            Lorem Ipsum is simply dummy
+            printing and typesetting industry
+          </p>
+        </div>
+
+        <div class="price">
+          89
+        </div>
+      </div>
+
+      <div class="product-sidebar">
+        <button class="buy">
+          <span>BUY ITEM</span>
+        </button>
+
+        <button class="info">
+          <span>MORE INFO</span>
+        </button>
+
+        <button class="size">
+          <span>SIZES</span>
+        </button>
+
+        <button class="colors">
+          <span>
+            <a href="/" class="color black"></a>
+            <a href="/" class="color white"></a>
+            <a href="/" class="color red"></a>
+          </span>
+        </button>
+      </div>
+      </div>
+    </main>
+    <main role="main">
+  <div class="product">
+    <figure>
+      <img src="https://raw.githubusercontent.com/itbruno/productpreview/master/assets/img/t-shirt.jpg" alt="Product" class="product-image"/>
+        </figure>
+
+      <div class="product-description">
+
+        <div class="info">
+          <h1>LOREM IPSUM</h1>
+          <p>
+            Lorem Ipsum is simply dummy
+            printing and typesetting industry
+          </p>
+        </div>
+
+        <div class="price">
+          89
+        </div>
+      </div>
+
+      <div class="product-sidebar">
+        <button class="buy">
+          <span>BUY ITEM</span>
+        </button>
+
+        <button class="info">
+          <span>MORE INFO</span>
+        </button>
+
+        <button class="size">
+          <span>SIZES</span>
+        </button>
+
+        <button class="colors">
+          <span>
+            <a href="/" class="color black"></a>
+            <a href="/" class="color white"></a>
+            <a href="/" class="color red"></a>
+          </span>
+        </button>
+      </div>
+      </div>
+    </main>
+    <main role="main">
+  <div class="product">
+    <figure>
+      <img src="https://raw.githubusercontent.com/itbruno/productpreview/master/assets/img/t-shirt.jpg" alt="Product" class="product-image"/>
+        </figure>
+
+      <div class="product-description">
+
+        <div class="info">
+          <h1>LOREM IPSUM</h1>
+          <p>
+            Lorem Ipsum is simply dummy
+            printing and typesetting industry
+          </p>
+        </div>
+
+        <div class="price">
+          89
+        </div>
+      </div>
+
+      <div class="product-sidebar">
+        <button class="buy">
+          <span>BUY ITEM</span>
+        </button>
+
+        <button class="info">
+          <span>MORE INFO</span>
+        </button>
+
+        <button class="size">
+          <span>SIZES</span>
+        </button>
+
+        <button class="colors">
+          <span>
+            <a href="/" class="color black"></a>
+            <a href="/" class="color white"></a>
+            <a href="/" class="color red"></a>
+          </span>
+        </button>
+      </div>
+      </div>
+    </main>
+
+
+    <main role="main">
+  <div class="product">
+    <figure>
+      <img src="https://raw.githubusercontent.com/itbruno/productpreview/master/assets/img/t-shirt.jpg" alt="Product" class="product-image"/>
+        </figure>
+
+      <div class="product-description">
+
+        <div class="info">
+          <h1>LOREM IPSUM</h1>
+          <p>
+            Lorem Ipsum is simply dummy
+            printing and typesetting industry
+          </p>
+        </div>
+
+        <div class="price">
+          89
+        </div>
+      </div>
+
+      <div class="product-sidebar">
+        <button class="buy">
+          <span>BUY ITEM</span>
+        </button>
+
+        <button class="info">
+          <span>MORE INFO</span>
+        </button>
+
+        <button class="size">
+          <span>SIZES</span>
+        </button>
+
+        <button class="colors">
+          <span>
+            <a href="/" class="color black"></a>
+            <a href="/" class="color white"></a>
+            <a href="/" class="color red"></a>
+          </span>
+        </button>
+      </div>
+      </div>
+    </main>
+    <main role="main">
+  <div class="product">
+    <figure>
+      <img src="https://raw.githubusercontent.com/itbruno/productpreview/master/assets/img/t-shirt.jpg" alt="Product" class="product-image"/>
+        </figure>
+
+      <div class="product-description">
+
+        <div class="info">
+          <h1>LOREM IPSUM</h1>
+          <p>
+            Lorem Ipsum is simply dummy
+            printing and typesetting industry
+          </p>
+        </div>
+
+        <div class="price">
+          89
+        </div>
+      </div>
+
+      <div class="product-sidebar">
+        <button class="buy">
+          <span>BUY ITEM</span>
+        </button>
+
+        <button class="info">
+          <span>MORE INFO</span>
+        </button>
+
+        <button class="size">
+          <span>SIZES</span>
+        </button>
+
+        <button class="colors">
+          <span>
+            <a href="/" class="color black"></a>
+            <a href="/" class="color white"></a>
+            <a href="/" class="color red"></a>
+          </span>
+        </button>
+      </div>
+      </div>
+    </main>
+
+    <main role="main">
+  <div class="product">
+    <figure>
+      <img src="https://raw.githubusercontent.com/itbruno/productpreview/master/assets/img/t-shirt.jpg" alt="Product" class="product-image"/>
+        </figure>
+
+      <div class="product-description">
+
+        <div class="info">
+          <h1>LOREM IPSUM</h1>
+          <p>
+            Lorem Ipsum is simply dummy
+            printing and typesetting industry
+          </p>
+        </div>
+
+        <div class="price">
+          89
+        </div>
+      </div>
+
+      <div class="product-sidebar">
+        <button class="buy">
+          <span>BUY ITEM</span>
+        </button>
+
+        <button class="info">
+          <span>MORE INFO</span>
+        </button>
+
+        <button class="size">
+          <span>SIZES</span>
+        </button>
+
+        <button class="colors">
+          <span>
+            <a href="/" class="color black"></a>
+            <a href="/" class="color white"></a>
+            <a href="/" class="color red"></a>
+          </span>
+        </button>
+      </div>
+      </div>
+    </main>
+
+      </div>
+    </>
+  );
+};
+
+export default MobileProducts;
