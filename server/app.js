4b2f93ce06b9fe14b24a3d4e50bccd013088333b
Shivam <127921762+Itachi7011@users.noreply.github.com>
2024-03-22 22:39:42 +0530

    Update app.js

--- server/app.js
+++ server/app.js
@@ -14,11 +14,8 @@ const dotenv = require("dotenv");
 dotenv.config({});
 
 require("./Database/connection");
-const customerInfoSchema = require("./Database/Schema/customerSchema");
-
-
-
-
+const customerSchemaDB = require("./Database/Schema/customerSchema");
+const sellerSchemaDB = require("./Database/Schema/sellerSchema");
 
 app.post("/newCustomerRegistration", async (req, res) => {
   try {
@@ -29,8 +26,9 @@ app.post("/newCustomerRegistration", async (req, res) => {
     const DateOfBirth = req.body.dateOfBirth;
     const SubmittedDate = req.body.dateOfFormSubmission;
 
-    const AlreadyUsedDatabase = await customerInfoSchema.findOne({ email: Email });
-
+    const AlreadyUsedDatabase = await customerSchemaDB.findOne({
+      email: Email,
+    });
 
     function getAge(dateString) {
       var today = new Date();
@@ -47,45 +45,107 @@ app.post("/newCustomerRegistration", async (req, res) => {
       return age;
     }
 
-
-
     if (Password === Cpassword) {
-      if(Email === AlreadyUsedDatabase.email){
-        alert("Sorry! This email is already registered. \n Please use another email.");
+      if (AlreadyUsedDatabase) {
+        alert(
+          "Sorry! This email is already registered. \n Please use another email."
+        );
         res.redirect("/CustomerRegistration");
-      }else {
-        if(Email === AlreadyUsedDatabase.phoneNo_1){
-          alert("Sorry! This Phone No. is already registered. \n Please use another Phone No.");
-          res.redirect("/CustomerRegistration");
-        
-        }else{
-        const customerSchema = await new customerInfoSchema({
-        name: req.body.name,
-        email: Email,
-        phoneNo_1: PhoneNo_1,
-        fatherName: req.body.fatherName,
-        dateOfBirth: req.body.dateOfBirth,
-        age: getAge(DateOfBirth),
-        country: req.body.country,
-        state: req.body.state,
-        district: req.body.district,
-        fullAddress: req.body.fullAddress,
-        dateOfFormSubmission: SubmittedDate,
-        password: req.body.password,
-      });
+      } else {
+        const customerSchema = await new customerSchemaDB({
+          name: req.body.name,
+          email: Email,
+          phoneNo_1: PhoneNo_1,
+          accountStatus: "active",
+          accountType: "customer",
+          fatherName: req.body.fatherName,
+          dateOfBirth: req.body.dateOfBirth,
+          age: getAge(DateOfBirth),
+          country: req.body.country,
+          state: req.body.state,
+          district: req.body.district,
+          fullAddress: req.body.fullAddress,
+          dateOfFormSubmission: SubmittedDate,
+          password: req.body.password,
+        });
 
-      await customerSchema.save();
-      console.log("Saved in Database Successfully");
-       alert("New Employee Registered Successfully! \n Please Login to Continue");
-      res.redirect("/CustomerLogin");
+        await customerSchema.save();
+        console.log("Saved in Database Successfully");
+        alert(
+          "New Seller Registered Successfully! \n Please Login to Continue"
+        );
+        res.redirect("/CustomerLogin");
       }
-    }
-      
     } else {
-     alert("Sorry! Password And Confirm Passwords do not match.");
+      alert("Sorry! Password And Confirm Passwords do not match.");
     }
   } catch (err) {
-    console.log(` Error During Registering Scale 1 Employees --> ${err} `);
+    console.log(` Error During Registeration of New Customer --> ${err} `);
+  }
+});
+
+app.post("/newSellerRegistration", async (req, res) => {
+  try {
+    const Password = req.body.password;
+    const Cpassword = req.body.cpassword;
+    const Email = req.body.email;
+    const PhoneNo_1 = req.body.phoneNo_1;
+    const DateOfBirth = req.body.dateOfBirth;
+    const SubmittedDate = req.body.dateOfFormSubmission;
+
+    const AlreadyUsedDatabase = await sellerSchemaDB.findOne({ email: Email });
+
+    function getAge(dateString) {
+      var today = new Date();
+
+      var birthDate = new Date(dateString);
+
+      var age = today.getFullYear() - birthDate.getFullYear();
+
+      var m = today.getMonth() - birthDate.getMonth();
+
+      if (m < 0 || (m === 0 && today.getDate() < birthDate.getDate())) {
+        age--;
+      }
+      return age;
+    }
+
+    if (Password === Cpassword) {
+      if (AlreadyUsedDatabase) {
+        alert(
+          "Sorry! This email is already registered. \n Please use another email."
+        );
+      } else {
+        const sellerSchema = await new sellerSchemaDB({
+          name: req.body.name,
+          email: req.body.email,
+          phoneNo_1: PhoneNo_1,
+          accountStatus: "active",
+          accountType: "customer",
+          fatherName: req.body.fatherName,
+          dateOfBirth: req.body.dateOfBirth,
+          age: getAge(DateOfBirth),
+          country: req.body.country,
+          state: req.body.state,
+          district: req.body.district,
+          fullAddress: req.body.fullAddress,
+          products: req.body.products,
+          dateOfFormSubmission: SubmittedDate,
+          password: req.body.password,
+        });
+
+        await sellerSchema.save();
+        console.log("Saved in Database Successfully");
+        alert(
+          "New Seller Registered Successfully! \n Please Login to Continue"
+        );
+        res.redirect("/SellerLogin");
+      }
+    } else {
+      alert("Sorry! Password And Confirm Passwords do not match.");
+    }
+  } catch (err) {
+    console.log(` Error During Registration of New Seller --> ${err} `);
   }
 });
 
